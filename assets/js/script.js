ed44fe047449bcaed80603bcd755ed52dbd63ddf
wtNorris <106634123+wtNorris@users.noreply.github.com>
2022-07-26 14:38:41 -0500

    rough draft

---
+++ assets/js/script.js
@@ -0,0 +1,120 @@
+localStorage.clear();
+
+function findCity() {
+    var cityName = $("#cityName")[0].value.trim();
+
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=5133ceb0f2a1d71c6003e2c39af6728c";
+
+    fetch(apiURL).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+
+                $("#city-name")[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
+
+                $("#city-list").append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + cityName);
+
+                const lat = data.coord.lat;
+                const lon = data.coord.lon;
+
+                var latLonPair = lat.toString() + " " + lon.toString();
+
+                localStorage.setItem(cityName, latLonPair);
+
+                apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=5133ceb0f2a1d71c6003e2c39af6728c";
+
+                fetch(apiURL).then(function (newResponse) {
+                    if (newResponse.ok) {
+                        newResponse.json().then(function (newData) {
+                            getCurrentWeather(newData);
+                        })
+                    }
+                })
+            })
+        } else {
+            alert("City not found, please try again.");
+        }
+    })
+}
+
+function getListCity(coordinates) {
+    apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&exclude=minutely,hourly&units=imperial&appid=5133ceb0f2a1d71c6003e2c39af6728c";
+
+    fetch(apiURL).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                getCurrentWeather(data);
+            })
+        }
+    })
+}
+
+function getCurrentWeather(data) {
+    $(".results-panel").addClass("visible");
+
+    $("#currentIcon")[0].src = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png";
+    $("#temperature")[0].textContent = "Temperature: " + data.current.temp.toFixed(1) + " \u2109";
+    $("#humidity")[0].textContent = "Humidity: " + data.current.humidity + "% ";
+    $("#wind-speed")[0].textContent = "Wind Speed: " + data.current.wind_speed.toFixed(1) + " MPH";
+    $("#uv-index")[0].textContent = "  " + data.current.uvi;
+
+    if (data.current.uvi < 3) {
+        $("#uv-index").removeClass("moderate severe");
+        $("#uv-index").addClass("favorable");
+    } else if (data.current.uvi < 6) {
+        $("#uv-index").removeClass("favorable severe");
+        $("#uv-index").addClass("moderate");
+    } else {
+        $("#uv-index").removeClass("favorable moderate");
+        $("#uv-index").addClass("severe");
+    }
+
+    getFutureWeather(data);
+}
+
+function getFutureWeather(data) {
+    for (var i = 0; i < 5; i++) {
+        var futureWeather = {
+            date: convertUnixTime(data, i),
+            icon: "http://openweathermap.org/img/wn/" + data.daily[i + 1].weather[0].icon + "@2x.png",
+            temp: data.daily[i + 1].temp.day.toFixed(1),
+            wind: data.daily[i + 1].wind_speed,
+            humidity: data.daily[i + 1].humidity
+        }
+
+        var currentSelector = "#day-" + i;
+        $(currentSelector)[0].textContent = futureWeather.date;
+        currentSelector = "#img-" + i;
+        $(currentSelector)[0].src = futureWeather.icon;
+        currentSelector = "#temp-" + i;
+        $(currentSelector)[0].textContent = "Temp: " + futureWeather.temp + " \u2109";
+        currentSelector = "#wind-" + i;
+        $(currentSelector)[0].textContent = "Wind: " + futureWeather.wind + " MPH"
+        currentSelector = "#hum-" + i;
+        $(currentSelector)[0].textContent = "Humidity: " + futureWeather.humidity + "%";
+    }
+}
+
+function convertUnixTime(data, index) {
+    const dateObject = new Date(data.daily[index + 1].dt * 1000);
+
+    return (dateObject.toLocaleDateString());
+}
+
+$("#searchBtn").on("click", function (event) {
+    event.preventDefault();
+
+    findCity();
+
+    $("form")[0].reset();
+})
+
+$(".city-list-box").on("click", ".city-name", function () {
+
+    var coordinates = (localStorage.getItem($(this)[0].textContent)).split(" ");
+    coordinates[0] = parseFloat(coordinates[0]);
+    coordinates[1] = parseFloat(coordinates[1]);
+
+    $("#city-name")[0].textContent = $(this)[0].textContent + " (" + moment().format('M/D/YYYY') + ")";
+
+    getListCity(coordinates);
+})
